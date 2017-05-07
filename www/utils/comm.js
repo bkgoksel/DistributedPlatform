f7e862d05db51c7b082c3284ad74bb70a101d0d6
mclonce <lonce.wyse@nus.edu.sg>
2017-05-07 14:38:00 -0700

    updated slorksounds to work on local and remote servers

--- www/utils/comm.js
+++ www/utils/comm.js
@@ -8,7 +8,7 @@ define(
 			if ((window.document.location.hostname).includes("sonicthings.org")){
 				host="sonicthings.org:9999";//+window.document.location.hostname;
 			} else {
-				host="mockserver.com:9000"
+				host="mockserver.com:9001"
 			}
 
 			var ws = new WebSocket('wss://' + host);
